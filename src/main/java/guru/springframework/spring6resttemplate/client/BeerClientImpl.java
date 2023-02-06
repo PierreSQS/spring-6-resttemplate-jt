4c55255e29a32afb6c448e95842efb38bc5b0b93
PierreSQS <nomo_messina@web.de>
2023-02-06 14:52:31 +0100

    Sec19_Chap193-Get List as a JSON String: related code

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,14 +1,35 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot, 06.02.2023.
  */
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private static final String API_URL = "/api/v1/beer";
+    private static final String BASE_URL = "http://localhost:8080";
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> respEntity =
+                restTemplate.getForEntity(BASE_URL+API_URL, String.class);
+
+        System.out.printf("The Response Body: %s%n",respEntity.getBody());
+
         return null;
     }
 }
