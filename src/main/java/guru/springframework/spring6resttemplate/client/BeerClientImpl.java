d05f6dfb76520c1aac3046a20a3097f844e8d41d
PierreSQS <nomo_messina@web.de>
2024-06-02 07:34:54 +0200

    Sec19_Chap194: implemented a Response in a Map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 /**
  * Modified by Pierrot on 01-06-2024.
  */
@@ -17,16 +19,24 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class BeerClientImpl implements BeerClient {
 
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String API_URL = "/api/v1/beer";
+
     private final RestTemplateBuilder restTemplateBuilder;
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> responseEntity =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        ResponseEntity<String> stringResponseEntity =
+                restTemplate.getForEntity(BASE_URL + API_URL, String.class);
 
-        log.info("the Response content: {}", responseEntity.getBody());
+        log.info("the Response content: {}", stringResponseEntity.getBody());
+
+        ResponseEntity<Map> mapResponseEntity =
+                restTemplate.getForEntity(BASE_URL + API_URL, Map.class);
+
+        log.info("the Response content: {}", mapResponseEntity.getBody());
 
         return null;
     }
