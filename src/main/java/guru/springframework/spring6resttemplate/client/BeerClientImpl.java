720d479ce0351858674ae86e352b40b03b74ad98
PierreSQS <nomo_messina@web.de>
2024-06-02 07:19:31 +0200

    Sec19_Chap193: implemented the client service and test

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,14 +1,33 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
- * Created by Pierrot on 01-06-2024.
+ * Modified by Pierrot on 01-06-2024.
  */
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> responseEntity =
+                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+
+        log.info("the Response content: {}", responseEntity.getBody());
+
         return null;
     }
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,15 +1,28 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 /**
- * Created by Pierrot on 01-06-2024.
+ * Modified by Pierrot on 02-06-2024.
  */
 @SpringBootTest
 class BeerClientImplTest {
 
+    @Autowired
+    BeerClient beerClient;
+
     @Test
     void listBeers() {
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+
+// has a weird behavior !!
+//        assertThat(beerDTOS.getContent().size()).isGreaterThan(2400);
+//        assertThat(beerDTOS.getSize()).isEqualTo(25);
+
+
     }
 }
