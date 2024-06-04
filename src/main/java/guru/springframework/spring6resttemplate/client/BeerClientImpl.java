3ed039f5714578c1791d63b965309a8666a2de64
PierreSQS <nomo_messina@web.de>
2024-06-04 16:50:59 +0200

    Sec19_Chap199: added a UriComponentsBuilder

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 
@@ -20,7 +21,7 @@ import java.util.Objects;
 @Service
 public class BeerClientImpl implements BeerClient {
 
-    public static final String API_URL = "/api/v1/beer";
+    public static final String BEER_API_URL = "/api/v1/beer";
 
     private final RestTemplateBuilder restTemplateBuilder;
 
@@ -28,8 +29,10 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(BEER_API_URL);
+
         ResponseEntity<BeerDTOPageImpl> pageRespEntity =
-                restTemplate.getForEntity(API_URL, BeerDTOPageImpl.class);
+                restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
 
         BeerDTOPageImpl body = pageRespEntity.getBody();
 
